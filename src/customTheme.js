e3c61e456fd4d721eb0f22f2d357e34b85bad523
Lesley <lesleylai@lesleylai.info>
2020-02-14 21:08:28 -0700

    complete talk

---
+++ src/customTheme.js
@@ -0,0 +1,13 @@
+import { nightOwl } from "@code-surfer/themes";
+
+const customTheme = {
+  ...nightOwl,
+  styles: {
+    ...nightOwl,
+    Slide: {
+      padding: "0 10%"
+    }
+  }
+};
+
+export default customTheme;
